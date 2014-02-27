52d2137955e66a58ca1868599c6e3e16bcc6ae90
Blackburn <blackburn2929@gmail.com>
2014-02-27 11:13:38 -0600

    Re-add SmartAssV2 gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -398,6 +398,10 @@ extern struct cpufreq_governor cpufreq_gov_asswax;
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
+
 #endif
 
 
